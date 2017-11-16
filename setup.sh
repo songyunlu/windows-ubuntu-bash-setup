e5180f452d51ebedf30a2f2da247cade88e993ce
songyunlu <gn00023040@gmail.com>
2017-11-15 21:09:31 -0800

    Installed 'bc' to fix sbt dependency issue.

--- setup.sh
+++ setup.sh
@@ -242,6 +242,7 @@ echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.li
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 sudo apt-get update
 sudo apt-get install -y sbt
+sudo apt-get install -y bc
 
 # maven
 sudo apt-get install -y maven
