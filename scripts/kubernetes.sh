37affe377beec3013046e856c823a74430e07cf6
songyunlu <gn00023040@gmail.com>
2018-06-08 15:43:48 +0800

    Updated kubernetes.sh.

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -14,14 +14,14 @@ pip install kube-shell --user $(whoami)
 # kubectx
 git clone https://github.com/ahmetb/kubectx.git "${HOME}/.kubectx"
 sudo ln -s ${HOME}/.kubectx/kubectx /usr/local/bin/kubectx
-echo 'alias kctx="kubectx"' >> ${HOME}/.zshrc
+echo 'alias kx="kubectx"' >> ${HOME}/.zshrc
 
 # kubens
 sudo ln -s ${HOME}/.kubectx/kubens /usr/local/bin/kubens
 sudo chmod a+x /usr/local/bin/kubectx /usr/local/bin/kubens
-echo 'alias kns="kubens"' >> ${HOME}/.zshrc
+echo 'alias kn="kubens"' >> ${HOME}/.zshrc
 
 # kube-ps1
 git clone https://github.com/jonmosco/kube-ps1.git ${HOME}/.kube-ps1
 echo 'source ${HOME}/.kube-ps1/kube-ps1.sh' >> ${HOME}/.zshrc
-echo 'PROMPT="$(kube_ps1)"$PROMPT' >> ${HOME}/.zshrc
+echo "PROMPT='\$(kube_ps1)'$PROMPT" >> ${HOME}/.zshrc
