6276da517a92c54b33eb03ec549c53e5d67e8313
songyunlu <gn00023040@gmail.com>
2019-03-14 22:49:32 +0800

    Set JAVA_HOME in the installation of openjdk.

--- scripts/openjdk.sh
+++ scripts/openjdk.sh
@@ -4,3 +4,5 @@ sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install -y openjdk-${JAVA_VERSION}-jdk
 java -version
+
+echo 'export JAVA_HOME="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-amd64/jre"' >> ${HOME}/.zshrc
