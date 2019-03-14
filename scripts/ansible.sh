a8b1fea2929bbd8e66af17650997b866a5647852
songyunlu <gn00023040@gmail.com>
2019-03-14 22:39:13 +0800

    Install ansible via pip intead of apt-get.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
+pip install --user ansible
 ansible --version
