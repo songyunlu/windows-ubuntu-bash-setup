a5356d13c88c85436d94a5a8d4333249c25005a4
songyunlu <gn00023040@gmail.com>
2019-03-15 15:18:27 +0800

    Upgraded vagrant version.

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export KUBERNETES_VERSION=1.10.3
-export VAGRANT_VERSION=2.2.3
+export VAGRANT_VERSION=2.2.4
 export GRADLE_VERSION=5.2.1
 export JAVA_VERSION=8
 export GOLANG_VERSION=1.12.1
