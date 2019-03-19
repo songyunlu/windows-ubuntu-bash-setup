c1d55c58b796f0d81d8954a3d8c3f78fc6812a32
songyunlu <gn00023040@gmail.com>
2019-03-19 12:18:20 +0800

    Added WSL configuration file to change the mount point of Windows file system and the file permisssion of mounted files.

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -8,3 +8,11 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y zip unzip htop jq
 
 echo 'export PATH="$PATH:$HOME/.local/bin"' >> ${HOME}/.zshrc
+
+# chnage the mount point of windows file system to /c instead of /mnt/c
+# The options = "metadata" line is not necessary but it will fix folder and file permissions on WSL mounts so everything isn’t 777 all the time within the WSL mounts.
+cat <<EOF > /etc/wsl.conf
+[automount]
+root = /
+options = "metadata"
+EOF
